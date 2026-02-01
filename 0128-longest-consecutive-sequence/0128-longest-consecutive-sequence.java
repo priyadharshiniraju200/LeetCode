de4d595e4c66f6d39f5ebec7cef14017d11b567c
priyadharshiniraju200 <priyadharshiniraju2006@gmail.com>
2026-02-02 00:29:09 +0530

    Time: 33 ms (31.89%), Space: 95.8 MB (48.4%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
@@ -1,13 +1,16 @@
 class Solution {
     public int longestConsecutive(int[] nums) {
-        Set<Integer> set = new HashSet<>();
-        for (int num : nums) set.add(num);
+         Set<Integer> set = new HashSet<>();
+        for (int num : nums) set.add(num); // Add all numbers to set
         
         int maxLen = 0;
         for (int num : set) {
-            if (!set.contains(num - 1)) { // start of a sequence
+            // Only start counting if 'num' is the start of a sequence
+            if (!set.contains(num - 1)) {
                 int currentNum = num;
                 int streak = 1;
+                
+                // Count consecutive numbers
                 while (set.contains(currentNum + 1)) {
                     currentNum++;
                     streak++;
@@ -16,6 +19,19 @@ class Solution {
             }
         }
         return maxLen;
+    }
+
+    // Main method to test the code
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        
+        int[] nums1 = {100, 4, 200, 1, 3, 2};
+        System.out.println("Longest consecutive sequence length: " + sol.longestConsecutive(nums1));
+        // Output: 4 (sequence: 1,2,3,4)
+
+        int[] nums2 = {0, 3, 7, 2, 5, 8, 4, 6, 0, 1};
+        System.out.println("Longest consecutive sequence length: " + sol.longestConsecutive(nums2));
+
         
     }
 }
