345e6ac957dc0d72aad08b7166fa62ae12470a61
priyadharshiniraju200 <priyadharshiniraju2006@gmail.com>
2026-02-03 23:00:58 +0530

    Time: 40 ms (6.49%), Space: 79.2 MB (95.94%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
@@ -2,10 +2,13 @@ class Solution {
     public int findDuplicate(int[] nums) {
         Arrays.sort(nums);
         int dup=0;
-        for(int fast=0;fast<nums.length-1;fast++){
-            if(nums[fast]==nums[fast+1]){
+        int slow=0;
+        for(int fast=1;fast<=nums.length-1;fast++){
+            if(nums[slow]!=nums[fast]){
+                slow++;
+            }
+            if(nums[fast]==nums[slow]){
                 dup=nums[fast];
-
             }
         }
         return dup;  
