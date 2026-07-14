2431729147a0a9891dadd51a458f3ded6af3af7b
priyadharshiniraju200 <priyadharshiniraju2006@gmail.com>
2026-07-14 12:28:14 +0530

    Time: 0 ms (100%), Space: 48.5 MB (14.12%) - LeetHub

--- 0704-binary-search/0704-binary-search.java
+++ 0704-binary-search/0704-binary-search.java
@@ -3,17 +3,17 @@ class Solution {
         int left=0;
         int right=nums.length-1;
         while(left<=right){
-            int mid=(left+right)/2;
+            int mid=left+(right-left)/2;
             if(nums[mid]==target){
                 return mid;
             }
             else if(nums[mid]<target){
                 left=mid+1;
             }
-            else if(nums[mid]>target){
+            else{
                 right=mid-1;
             }
         }
-       return -1; 
+        return -1;
     }
 }
