d8045eb02e406da4bd9e22f1bc434e7d8dc130e8
priyadharshiniraju200 <priyadharshiniraju2006@gmail.com>
2026-08-17 10:54:46 +0530

    Time: 29 ms (78.97%), Space: 95.3 MB (66.38%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
@@ -1,37 +1,21 @@
 class Solution {
     public int longestConsecutive(int[] nums) {
-         Set<Integer> set = new HashSet<>();
-        for (int num : nums) set.add(num); // Add all numbers to set
-        
-        int maxLen = 0;
-        for (int num : set) {
-            // Only start counting if 'num' is the start of a sequence
-            if (!set.contains(num - 1)) {
-                int currentNum = num;
-                int streak = 1;
-                
-                // Count consecutive numbers
-                while (set.contains(currentNum + 1)) {
-                    currentNum++;
-                    streak++;
+        HashSet<Integer> set=new HashSet<>();
+        for(int num:nums){
+            set.add(num);
+        }
+        int maxlen=0;
+        for(int n:set){
+            if(!set.contains(n-1)){
+                int currnum=n;
+                int count=1;
+                while(set.contains(currnum+1)){
+                    currnum++;
+                    count++;
                 }
-                maxLen = Math.max(maxLen, streak);
+                maxlen=Math.max(maxlen,count);
             }
         }
-        return maxLen;
-    }
-
-    // Main method to test the code
-    public static void main(String[] args) {
-        Solution sol = new Solution();
-        
-        int[] nums1 = {100, 4, 200, 1, 3, 2};
-        System.out.println("Longest consecutive sequence length: " + sol.longestConsecutive(nums1));
-        // Output: 4 (sequence: 1,2,3,4)
-
-        int[] nums2 = {0, 3, 7, 2, 5, 8, 4, 6, 0, 1};
-        System.out.println("Longest consecutive sequence length: " + sol.longestConsecutive(nums2));
-
-        
+        return maxlen;
     }
 }
